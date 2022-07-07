81e00a1c8cd53a8f80264221f0f675f214ce23ba
Matt Hook <hookenz@gmail.com>
2022-07-07 14:19:09 +1200

    fix another typo

--- compose/internal/composeplugin/composeplugin.go
+++ compose/internal/composeplugin/composeplugin.go
@@ -77,7 +77,7 @@ func (wrapper *PluginWrapper) Pull(ctx context.Context, workingDir, host, projec
 	return err
 }
 
-// Command exectue a docker-compose commanåd
+// Command exectue a docker-compose command
 func (wrapper *PluginWrapper) command(command composeCommand, workingDir, host, projectName, envFilePath string) ([]byte, error) {
 	program := utils.ProgramPath(wrapper.binaryPath, "docker-compose")
 
