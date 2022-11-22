c702923f8ef5df5e06ec09de40cfda2a50afe1fa
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-11-22 13:56:48 +0200

    style: fix doc for new compose deployer

--- compose/compose.go
+++ compose/compose.go
@@ -5,8 +5,7 @@ import (
 	"github.com/portainer/docker-compose-wrapper/compose/internal/composeplugin"
 )
 
-// NewComposeDeployer will try to create a wrapper for docker-compose binary
-// if it's not availbale it will try to create a wrapper for docker-compose plugin
+// NewComposeDeployer will try to create a wrapper for docker-compose plugin
 func NewComposeDeployer(binaryPath, configPath string) (libstack.Deployer, error) {
 	return composeplugin.NewPluginWrapper(binaryPath, configPath)
 }
